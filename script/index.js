3723b742f9a88958999752f6f10c0a74fe7bef6f
victxrmarin <victormarin.dev@gmail.com>
2024-03-13 16:03:00 -0300

    a

--- script/index.js
+++ script/index.js
@@ -1,1 +1,9 @@
+function validaBusca() {
+    if(document.querySelector('#q').value == '') {
+        document.querySelector('')
+        alert('Não podia ter deixado em branco a busca!')
+        return false
+    }
+}
 
+document.querySelector('#form-busca').onsubmit = validaBusca()
